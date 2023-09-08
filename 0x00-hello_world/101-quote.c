3f95ba72c7002af1b3b11297830779fda202236c
evansboateng954 <evansboateng954@gmail.com>
2023-09-08 02:32:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * main - Entry Point
  *
  * Return: Always 0 (success)
  */
-int main()
+int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	char text[] = "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, text, sizeof(message) - 1);
+	return (1);
 }
