86b7663c10b4d1943a96419828946ca3efa67c4b
evansboateng954 <evansboateng954@gmail.com>
2023-09-08 01:24:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main()
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
