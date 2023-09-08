ca69d6d93f96fe46b1fe1347da8a7dc895169cb6
evansboateng954 <evansboateng954@gmail.com>
2023-09-08 02:36:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char text[] = "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n";
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, text, sizeof(text) - 1);
 	return (1);
