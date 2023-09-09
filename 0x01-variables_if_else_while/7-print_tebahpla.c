1b56ba5b8909c8d8cbdf7b4f7ddc4ea4af94cb7b
evansboateng954 <evansboateng954@gmail.com>
2023-09-09 02:02:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; i++)
+		putchar (i);
+	putchar ('\n');
+	return (0);
+}
