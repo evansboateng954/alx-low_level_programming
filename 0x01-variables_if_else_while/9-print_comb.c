b0e92df201c24d1db8447780b72fab10eaf11150
evansboateng954 <evansboateng954@gmail.com>
2023-09-09 04:39:39 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+		putchar (',');
+	}
+	putchar ('\n');
+	return (0);
+}
