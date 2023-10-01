fd6461900d941a3559daf1f990ea5ae67633faa4
evansboateng954 <evansboateng954@gmail.com>
2023-10-01 08:24:37 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: the number of command line arguments
+ * @argv: an array of command line arguments
+ * Return: a new the name of the file
+*/
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
+
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+char print_name(char *s);
+int _putchar(char c);
+#endif
