d9511e0bcaaf8256c8ffa19bf5568c3c236064f8
evansboateng954 <evansboateng954@gmail.com>
2023-10-04 13:49:22 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = (char *) malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	while (i < size)
 	{
 		*(ptr + i) = c;
