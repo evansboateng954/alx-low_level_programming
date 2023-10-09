ac35c96679db70d04c998b70d3ce124af7a0e536
evansboateng954 <evansboateng954@gmail.com>
2023-10-09 12:58:33 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog creates a  simple structure for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
