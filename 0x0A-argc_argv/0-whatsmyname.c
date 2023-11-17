e969b8d3fae189321a14b17b4d21b96b2c947293
Evans Boateng <evansboateng954@gmail.com>
2023-11-17 08:59:52 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * This program prints its name
+ * Return: 0 is returned
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
