7b49db1cf3c72ba4123d261afcb74368b233ed37
Evans Boateng <evansboateng954@gmail.com>
2023-11-17 09:19:45 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * This program prints the  number of arguments it recieves
+ * Return: 0 is returned
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int  argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d \n", argc);
+	return (0);
+}
