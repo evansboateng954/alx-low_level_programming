bcda0bfb4af3841ef73f88740e2e84bc6d8a7933
Evans Boateng <evansboateng954@gmail.com>
2023-11-27 14:13:08 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
-
-/**
- * _putchar - prints a char
- * @c: The character to print
- * Return: on sucess (1)
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * main - Entry point
  *
- * Description: This program prints the nam of
- * the file it was compiled from
- * Return: Always 0
+ * Description: This program prints the name of the
+ * file it was compiled from
+ * Return: always 0
  */
 int main(void)
 {
-	int i = 0;
-
-
-	while (__FILE__[i] != '\0')
-	{
-		_putchar(__FILE__[i]);
-		i++;
-	}
-	_putchar('\n');
-
+	printf("%s\n", __FILE__);
 	return (0);
 }
