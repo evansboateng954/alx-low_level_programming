1a4a518e50535b7c0d8fe92bdac55fd918636ccf
Evans Boateng <evansboateng954@gmail.com>
2023-11-28 11:22:51 -0800

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdio.h>
 /**
  * struct dog - is a dog data type
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
