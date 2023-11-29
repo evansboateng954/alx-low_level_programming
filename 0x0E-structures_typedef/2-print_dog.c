f05a1ca1c047006466d48189f6e6f2963ca0a303
Evans Boateng <evansboateng954@gmail.com>
2023-11-28 16:06:19 -0800

    hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,5 @@ void print_dog(struct dog *d)
 	else if (!d->owner)
 		d->owner = "(nil)";
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
