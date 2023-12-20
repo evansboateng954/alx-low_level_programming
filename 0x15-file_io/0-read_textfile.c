a465fafa10b91c479887b35cb609a5d085e9d3e4
Evans Boateng <evansboateng954@gmail.com>
2023-12-20 15:42:24 -0800

    read from a file and write to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,6 @@
  * if the file cannot be opened return 0 or if
  * write fails or filename equals NULL return 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *ch;
@@ -19,12 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 
 	if (fd < 0)
 		return (0);
-	
 	ch = malloc(letters);
 
 	if (!ch)
@@ -42,15 +39,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 /* write a character at a time */
 
-	while((fd = write(STDIN_FILENO, &ch[i], 1)) > 0)
+	while ((fd = write(STDIN_FILENO, &ch[i], 1)) > 0)
 	{
-		
 		i++;
 		if (fd < 0)
 		{
-			free (ch);
+			free(ch);
 			close(fd);
-
 			return (0);
 		}
 	}
