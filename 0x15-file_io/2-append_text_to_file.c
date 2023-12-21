04d4efe6bbe37d47ffa8a91d0094b64f8a117a7b
Evans Boateng <evansboateng954@gmail.com>
2023-12-21 00:35:22 -0800

    append to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,13 +14,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	
+	if (!text_content)
+		return (1);
 
 	fd = open(filename, O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		close(fd);
 
 	for (i = 0; text_content[i] != 0; i++)
 	{
