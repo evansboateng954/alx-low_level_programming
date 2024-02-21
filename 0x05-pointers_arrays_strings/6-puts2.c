da7aa2b6036b2efdbdab59fb7cafa7ea3bf6b9b3
evansboateng954 <evansboateng954@gmail.com>
2024-02-20 23:57:56 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts2 - prints the next 2 chars till endl starting from the first
+ * @str: pointer to the string to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
+
